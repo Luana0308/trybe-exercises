94b64d74624de5b04273b10bce709fa71619287f
Luana <luanaff-2008@hotmail.com>
2021-11-08 11:14:04 -0300

    exercicio 11 realizado

--- fundamentos/bloco-04-javascript-logica-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-javascript-logica-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -183,3 +183,37 @@ let valorLucro = (valorVendaProduto - valorCustoTotal) * 1000
 console.log(valorLucro)
 
 //exercicio11
+
+let salarioBruto = 3000
+let INSS; 
+let impostoRenda;
+if(salarioBruto <= 1556.94){
+INSS = salarioBruto *0.08
+}
+else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92){
+INSS = salarioBruto * 0.09
+}
+else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82){
+INSS = salarioBruto * 0.11
+}
+else if (salarioBruto >= 5189.82){
+INSS = 570.88
+}
+
+let salarioINSS = salarioBruto - INSS
+if(salarioINSS <= 1903.98){
+impostoRenda = 0
+}
+else if(salarioINSS>= 1903.99 && salarioINSS<= 2826.65){
+impostoRenda = (salarioINSS * 0.075) - 142.80
+}
+else if(salarioINSS>= 2826.66 && salarioINSS<= 3751.05){
+impostoRenda = (salarioINSS * 0.15) - 354.80
+}
+else if(salarioINSS>= 3751.06 && salarioINSS<= 4664.68){
+impostoRenda = (salarioINSS * 0.225) - 636.13 
+}
+else if (salarioINSS >= 4664.68){
+impostoRenda = (salarioINSS * 0.275) - 869.36
+}
+console.log("salario: " + (salarioINSS - impostoRenda))
