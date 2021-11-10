caf7503fea2561a24161e106fb99b6299a5d0c98
Luana <luanaff-2008@hotmail.com>
2021-11-10 12:15:22 -0300

    exercicios dia 5

---
+++ fundamentos/bloco-04-javascript-logica-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -0,0 +1,33 @@
+/*let asterisco = "*";
+let multiplicado = 5;
+let resultado = " ";
+for (let index = 0; index < multiplicado; index++) {
+    resultado = resultado + asterisco; 
+}; 
+for (let index = 0; index < multiplicado; index++) {
+    console.log(resultado)
+}*/
+
+/*let asterisco = "*"
+let multiplicado = 5;
+let resultado = " "
+for (let index = 0; index < multiplicado; index++) {
+    resultado = resultado + asterisco
+    console.log(resultado)  
+}*/
+
+/*let asterisco = "*"
+let n = 5;
+let espaco = " "
+let linha = n
+for (let linha = 0; linha < n; linha +=1){
+    let piramide = ""
+    for(i=1; i <= n; i+=1){
+        if(i < n-linha){
+            piramide =piramide + espaco
+        }else{
+            piramide = piramide + asterisco
+        }
+    }
+    console.log(piramide)
+}*/
