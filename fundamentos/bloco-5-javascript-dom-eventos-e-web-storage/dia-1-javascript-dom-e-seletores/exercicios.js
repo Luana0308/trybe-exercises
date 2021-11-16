c3f0f2966fe2dd1a297ce3044b1056fe9b950dbd
Luana <luanaff-2008@hotmail.com>
2021-11-16 18:37:49 -0300

    exemplo 1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios.js
@@ -0,0 +1,5 @@
+//exemplo 1
+/*document.getElementById("page-title").innerText = "Filme Favorito: Click"
+document.getElementById("second-paragraph").innerText = "Os filmes que eu mais gosto são de comedia romantica"
+document.getElementById("subtitle").innerText = "Estilo de filmes"
+document.getElementsByClassName("paragrafo")[0].innerText = "Esse é meu filme favorito porque é sobre familia"*/
