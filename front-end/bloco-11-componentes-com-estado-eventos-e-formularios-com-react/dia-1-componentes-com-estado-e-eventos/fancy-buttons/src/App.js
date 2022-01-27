e309f1c01d98513e4bfd96a4e72b7bb6971ecdb5
Luana <luanaff-2008@hotmail.com>
2022-01-26 21:07:55 -0300

    exercicios de fixação

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -2,40 +2,50 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-
-function handleClick2() {
-  console.log('Clicou no botão 2!')
-}
-function handleClick3() {
-  console.log('Clicou no botão 3!')
-}
-
 class App extends React.Component {
   constructor() {
     super()
+    this.state = {
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0
+    }
     this.handleClick1 = this.handleClick1.bind(this)
     this.handleClick2 = this.handleClick2.bind(this)
     this.handleClick3 = this.handleClick3.bind(this)
   }
   handleClick1() {
-    console.log(this)
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques1: estadoAnterior.numeroDeCliques1 + 1
+    }))
     console.log('Clicou no botão 1!')
   }
   handleClick2() {
-    console.log(this)
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques2: estadoAnterior.numeroDeCliques2 + 1
+    }))
     console.log('Clicou no botão 2!')
   }
   handleClick3() {
-    console.log(this)
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques3: estadoAnterior.numeroDeCliques3 + 1
+    }))
     console.log('Clicou no botão 3!')
+  }
+  getButtonColor(botao) {
+    if (botao % 2 === 0) {
+      return 'pink'
+    } else {
+      return 'aqua'
+    }
   }
 
   render() {
       return (
         <div>
-          <button onClick={this.handleClick1}>Botão 1</button>
-          <button onClick={this.handleClick2}>Botão 2</button>
-          <button onClick={this.handleClick3}>Botão 3</button>
+          <button onClick={this.handleClick1} style={{backgroundColor: this.getButtonColor(this.state.numeroDeCliques1)}}>Botão One {this.state.numeroDeCliques1}</button>
+          <button onClick={this.handleClick2} style={{backgroundColor: this.getButtonColor(this.state.numeroDeCliques2)}}>Botão two {this.state.numeroDeCliques2}</button>
+          <button onClick={this.handleClick3} style={{backgroundColor: this.getButtonColor(this.state.numeroDeCliques3)}}>Botão three {this.state.numeroDeCliques3}</button>
         </div>
       )
        
