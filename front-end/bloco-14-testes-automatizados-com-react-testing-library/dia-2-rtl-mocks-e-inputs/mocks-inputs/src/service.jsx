2e6df2923362d2d2cffbdf95c859067f154f056f
Luana <luanaff-2008@hotmail.com>
2022-02-17 19:26:43 -0300

    exercicio 2

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/mocks-inputs/src/service.jsx
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/mocks-inputs/src/service.jsx
@@ -1,2 +1,4 @@
 const randomNumber = () => Math.floor(Math.random() * 101);
-module.exports = { randomNumber };
+const divisivelPorDois = () => (randomNumber() % 2) === 0;
+
+module.exports = { randomNumber, divisivelPorDois };
--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/mocks-inputs/src/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/mocks-inputs/src/service.test.js
@@ -7,3 +7,12 @@ test('teste se a função tem o retorno padrão de 10', () => {
     expect(service.randomNumber).toHaveBeenCalled();
     expect(service.randomNumber).toHaveBeenCalledTimes(1);
 })
+
+test('testar se a função foi chamada apenas uma unica vez, testar se foi aplicado a divisão', () => {
+    service.divisivelPorDois = jest.fn().mockImplementationOnce((a,b) => a/b)
+
+    expect(service.divisivelPorDois(10,5)).toBe(2)
+    expect(service.divisivelPorDois).toHaveBeenCalled();
+    expect(service.divisivelPorDois).toHaveBeenCalledTimes(1);
+
+})
