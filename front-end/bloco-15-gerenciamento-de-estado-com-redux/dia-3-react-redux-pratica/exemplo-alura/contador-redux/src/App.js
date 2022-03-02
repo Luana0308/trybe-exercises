a97b248ca42ae48db79feb11a0b3ecdb12a3ceec
Luana <luanaff-2008@hotmail.com>
2022-03-02 00:06:57 -0300

    criando mapstatetoprops

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-3-react-redux-pratica/exemplo-alura/contador-redux/src/App.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-3-react-redux-pratica/exemplo-alura/contador-redux/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import { connect } from 'react-redux'
+import { decrementarContador, incrementarContador } from './actions/contador';
 
-function App({ contador }) {
+function App({ contador, incrementar, decrementar }) {
   return (
-    <div className="App">
-      Contador: {contador}
-    </div>
+    <>
+      <div className="App">Contador: {contador}</div>
+      <button onClick={incrementar}>Incrementar</button>
+      <button onClick={decrementar}>Decrementar</button>
+    </>
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  contador: state.contadorReducer.contador,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  incrementar: () => dispatch(incrementarContador()),
+  decrementar: () => dispatch(decrementarContador()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
