c12f33b0c2c95bbf5a525dbdef4043a7b3ce723d
Luana <luanaff-2008@hotmail.com>
2022-07-05 09:46:15 -0300

    enum

---
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/emunExercise.ts
@@ -0,0 +1,33 @@
+enum diasSemana {
+    segunda = 1,
+    terca,
+    quarta,
+    quinta,
+    sexta,
+    sabado,
+    domingo
+
+}
+
+enum arcoIris {
+    verde = 'verde',
+    amarelo = 'amarelo',
+    azul = 'azul',
+    vermelho = 'vermelho',
+    roxo = 'roxo',
+}
+
+enum acoes {
+    salvar, 
+    imprimir, 
+    abrir, 
+    visualizar,
+    fechar
+}
+
+enum pontoCardial {
+    Norte = 'N', 
+    Leste = 'L', 
+    Sul = 'S',
+    Oeste = 'O'
+}
